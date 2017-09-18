c18b58ceba8b03ec63eb09fb26db9ce8dc1184c1
DefaultProfile <nathan.sterk@gmail.com>
2017-09-18 00:46:49 -0400

    Removed textbook examples and book

---
+++ asg1/script.js
@@ -0,0 +1,3 @@
+document.write("Hello, I am taking this class so that I can have a general understanding of how web applications work." +
+    "I took HTML a year and a half ago, so I'm a bit rusty" +
+    ".");
